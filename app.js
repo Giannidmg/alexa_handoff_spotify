6ff95ce52f6f455c46e62d1cd0a2c1b635365b0f
Giannidmg <givanniromeo@gmail.com>
2025-01-04 18:33:36 +0100

    END all flow for switch device

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 async function onLoad() {
+
+  // recuper informazioni dalla chiamata
   const urlParams = new URLSearchParams(window.location.search);
   const clientId = urlParams.get("client_id");
   const clientSecret = urlParams.get("client_secret");
+  const deviceName = decodeURIComponent(urlParams.get("device_name"));
+
+  // salvataggio in local storage
   localStorage.setItem("client_id", clientId);
   localStorage.setItem("client_secret", clientSecret);
+  localStorage.setItem("device_name", deviceName);
+
   document.getElementById("client_id").innerHTML = clientId;
 
-  const codeVerifier = generateRandomString(64);
-  localStorage.setItem("code_verifier", codeVerifier);
+  // const codeVerifier = generateRandomString(64);
+  // localStorage.setItem("code_verifier", codeVerifier);
 
+  // const hashed = await sha256(codeVerifier);
+  // const codeChallenge = base64encode(hashed);
 
-  const hashed = await sha256(codeVerifier);
-  const codeChallenge = base64encode(hashed);
-
-  //const redirectUri = "http://localhost:5500/callback";
+  //genero chiamata REst per AUTH
   const redirectUri = window.location.protocol + "//" + window.location.host + "/callback.html";
 
-  const scope = "user-read-private user-read-email user-read-playback-state";
+  const scope = `user-read-private
+                  user-read-email 
+                  user-read-playback-state 
+                  user-modify-playback-state
+                `;
   const authUrl = new URL("https://accounts.spotify.com/authorize");
-
-  // generated in the previous step
-  window.localStorage.setItem("code_verifier", codeVerifier);
 
   const params = {
     response_type: "code",
@@ -35,18 +42,27 @@ async function onLoad() {
 }
 
 async function onCallback() {
+
+  // recupero il code generato da URL
   const urlParams = new URLSearchParams(window.location.search);
   let code = urlParams.get("code");
   window.document.getElementById("code").innerHTML = code;
+
+  // salvo il codice in local storage
   localStorage.setItem("code", code);
+
+  // recupero i dati da local storage
   var clientId = localStorage.getItem("client_id");
   var clientSecret = localStorage.getItem("client_secret");
-  var codeVerifier = localStorage.getItem("code_verifier");
-  var authb64 = "Basic " + btoa(clientId + ':' + clientSecret);
-  console.log(authb64)
-  //const redirectUri = window.location.protocol + "//" + window.location.host + "/token.html";
-  var redirect_uri = "http://localhost:5500/callback.html";
+  var deviceName = localStorage.getItem("device_name");
 
+  // genero la Basic per Autenticazione
+  var authb64 = "Basic " + btoa(clientId + ':' + clientSecret);
+
+  var redirectUri = window.location.protocol + "//" + window.location.host + "/callback.html";
+  //var redirect_uri = "http://localhost:5500/callback.html";
+
+  // avvio la chiamata per il token
   const url = new URL("https://accounts.spotify.com/api/token");
 
   const payload = {
@@ -58,24 +74,37 @@ async function onCallback() {
     body: new URLSearchParams({
       "grant_type" : "authorization_code",
       "code" : code,
-      "redirect_uri": redirect_uri,
+      "redirect_uri": redirectUri,
     }),
   };
 
   const body = await fetch(url, payload);
   const response = await body.json();
 
-  console.log(response);
-
+  // salvo i token in local storage
   localStorage.setItem("access_token", response.access_token);
   localStorage.setItem("refresh_token", response.refresh_token);
+
+  const deviceUrl = new URL(window.location.protocol + "//" + window.location.host + "/getdevices.html");
+  const params = {
+    access_token: response.access_token,
+    device_name: deviceName
+  };
+
+  deviceUrl.search = new URLSearchParams(params).toString();
+  window.location.href = deviceUrl.toString();
+
 }
 
-async function getDevice(){
+async function getDevices(){
+
+  // recupero access token da URL
   const urlParams = new URLSearchParams(window.location.search);
-  var access_token = urlParams.get("access_token");
-  var auth = "Bearer " + access_token;
+  var accessToken = urlParams.get("access_token");
+  var deviceName = urlParams.get("device_name");
+  var auth = "Bearer " + accessToken;
   
+  // avvio recuper dei dispositivi
   const url = new URL("https://api.spotify.com/v1/me/player/devices");
   const payload = {
     method: "GET",
@@ -86,8 +115,44 @@ async function getDevice(){
 
   const body = await fetch(url, payload);
   const response = await body.json();
+  const devices = await response.devices;
 
-  console.log(response);
+  var deviceID = devices.map(dev => dev.name == deviceName ? dev.id : null ).filter(id => id != null)[0];
+
+  localStorage.setItem("device_id", deviceID);
+
+  const setDeviceUrl = new URL(window.location.protocol + "//" + window.location.host + "/setdevice.html");
+  const params = {
+    access_token: accessToken,
+    device_id: deviceID
+  };
+
+  setDeviceUrl.search = new URLSearchParams(params).toString();
+  window.location.href = setDeviceUrl.toString();
+
+
+}
+
+async function setNewDevice(){
+
+  // recupero access token da URL
+  const urlParams = new URLSearchParams(window.location.search);
+  var accessToken = urlParams.get("access_token");
+  var deviceID = urlParams.get("device_id");
+  var auth = "Bearer " + accessToken;
+  
+  // avvio recuper dei dispositivi
+  const url = new URL("https://api.spotify.com/v1/me/player");
+  const payload = {
+    method: "PUT",
+    headers: {
+      "Authorization": auth,
+      "Content-Type" : "application/json"
+    },
+    body: JSON.stringify({
+      "device_ids": [deviceID]
+    })
+  };
 
 }
 
